3d0c0537e426ee0ba155b9afe0e986c14634b93d
Mohtashim Javed <hafizmohtashim3157@gmail.com>
2025-09-20 09:42:13 +0500

    updated

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-
+// e-commerce site
+async function fetchApi() {
+    const fetch_data = await fetch(`https://api.escuelajs.co/api/v1/products`)
+    const data = await fetch_data.json()
+    console.log(data);
+    document.getElementById("article").innerHTML = `<img src="${data[1].images[0]}" alt="" />`
+    
+}
+fetchApi()
