e17ed0b10fa2f9ee5fa37722327557605cffd835
Mohtashim Javed <hafizmohtashim3157@gmail.com>
2025-09-20 09:54:12 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // e-commerce site
 async function fetchApi() {
-    const fetch_data = await fetch(`https://api.escuelajs.co/api/v1/products`)
+    const fetch_data = await fetch(`https://fakestoreapi.com/products/category/electronics`)
     const data = await fetch_data.json()
     console.log(data);
-    document.getElementById("article").innerHTML = `<img src="${data[1].images[0]}" alt="" />`
+    // document.getElementById("article").innerHTML = `<img src="${data[1].images[0]}" alt="" />`
     
 }
 fetchApi()
