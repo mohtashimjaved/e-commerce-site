bd882f8c26260f5937fd74e2dfe8b79e29c7fc63
Mohtashim Javed <hafizmohtashim3157@gmail.com>
2025-09-21 16:07:09 +0500

    updated

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
 // e-commerce site
-const api = `https://fakestoreapi.com`
+const api = `https://dummyjson.com`
 async function categoryApi() {
     const fetch_api = await fetch(`${api}/products/categories`)
     const data = await fetch_api.json()
     console.log(data);
     const category_div = document.getElementById("category_div")
     for (let i = 0; i < data.length; i++) {
-        category_div.innerHTML += `<a class="category">${data[i]}</a>`
+        category_div.innerHTML += `<a class="category" href="">${data[i].name}</a>`
+        
         
     }
 }
 categoryApi()
 async function productApi() {
-    const fetch_api = await fetch(`${api}/products`)
+    const fetch_api = await fetch(`${api}/products?limit=50&skip=0`)
     const data = await fetch_api.json()
     console.log(data);
+    const products = data.products
     const products_div = document.getElementById("products_div")
-    for (let i = 0; i < data.length; i++) {
-        products_div.innerHTML += `<a class="product">
+    for (let i = 0; i < products.length; i++) {
+        products_div.innerHTML += `<a class="product" href="">
         <div class=>
-        <div class="product_image"><img src="${data[i].image}"/></div>
+        <div class="product_image"><img src="${products[i].images[0]}"/></div>
         <div>
         <div>
-        ${data[i].title}
+        ${products[i].title}
         </div>
-        <div>$${data[i].price}</div>
-        <div>${data[i].description}</div>
+        <div>$${products[i].price}</div>
+        <div>${products[i].description}</div>
         </div>
         </div>
         </a>`
