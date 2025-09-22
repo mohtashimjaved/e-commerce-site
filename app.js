2ac5730929217a95a0f5b24c0b55e6179f4b0b7f
Mohtashim Javed <hafizmohtashim3157@gmail.com>
2025-09-23 04:07:16 +0500

    updated

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ async function categoryApi() {
         category_div.innerHTML += `<a class="category" href="./category.html?name=${data[i].name}">${data[i].name}</a>`
 
     }
-
     let title;
+    const categoryProductsMain = document.querySelector(".categoryProductsMain")
     if (window.location.pathname == "/category.html") {
         const search_params = new URLSearchParams(window.location.search);
         for (const [key, value] of search_params.entries()) {
@@ -22,30 +22,36 @@ async function categoryApi() {
         console.log(title);
         const current_category = data.find(categoryName => categoryName.name === title)
         console.log(current_category);
-        const categoryProductsDiv = document.getElementById("categoryProductsDiv");
-        const categoryProductApi = await fetch(current_category.url);
-        const categoryProductData = await categoryProductApi.json();
-        const categoryproducts = categoryProductData.products
-        console.log(categoryProductData);
-        for (let i = 0; i < categoryproducts.length; i++) {
-            categoryProductsDiv.innerHTML += `<a class="CategoryProducts">
-            <div class="product_inner">
-            <div class="product_image"><img src="${categoryproducts[i].images[0]}"/></div>
-            <div>
-            <div>
-            ${categoryproducts[i].title}
-            </div>
-            <div>$${categoryproducts[i].price}</div>
-            <div>${categoryproducts[i].description}</div>
-            </div>
-            </div>
-            </a>`
+        const breadcrumb_item_active = document.getElementById("breadcrumb_item_active")
+            const categoryProductApi = await fetch(current_category.url);
+            const categoryProductData = await categoryProductApi.json();
+            const categoryproducts = categoryProductData.products
+            console.log(categoryProductData);
+            const category_products_heading = document.getElementById("category_products_heading")
+            const categoryProductsDiv = document.getElementById("categoryProductsDiv")
+            category_products_heading.innerText = title
+            breadcrumb_item_active.innerText = title
+            for (let i = 0; i < categoryproducts.length; i++) {
+                categoryProductsDiv.innerHTML += `<a class="CategoryProducts">
+                <div class="product_inner" >
+                <div class="product_image"><img src="${categoryproducts[i].images[0]}"/></div>
+                <div class="product_details">
+                <div class="product_price">$${categoryproducts[i].price}<i class="fa-solid   fa-cart-plus"></i></div>
+                <div class="product_title">
+                ${categoryproducts[i].title}
+                </div>
+                <div class="product_descp">${categoryproducts[i].description}</div>
+                <div class="product_rating">Rating ${categoryproducts[i].rating}</div>
+                </div>
+                </div>
+                </a>`
+            }
         }
 
 
 
     }
-}
+
 categoryApi()
 async function productApi() {
     const fetch_api = await fetch(`${api}/products?limit=40&skip=77`)
